ea379cedf031d97c9b09abc0567fb0ef1af62117
jdgg33 <61071079+jdgg33@users.noreply.github.com>
2020-02-28 19:27:37 -0500

    Se crean Arreglos Indexados y Asociativos

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo("Clase 4 Bucles, Arreglos y Funciones <br><br>");
+
+
+//Array indexado, asigna por defecto el identificador o clave del valor ingresado.
+$arreglo=array("Juan","Maria","James","Sandra","David");
+echo("<br>");
+print_r($arreglo);
+echo("<br><br>................................<br>");
+
+//array asociativo
+
+$arreglo=array('usuario1'=>"Juan,",'usuario2'=>"Maria,",'usuario3'=>"James,",'usuario4'=>"Sandra,",'usuario5'=>"David,");
+echo("<br>");
+print_r($arreglo);
+
+
+
+?>
